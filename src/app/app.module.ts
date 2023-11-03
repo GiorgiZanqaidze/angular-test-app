065f3a32fa5c401ca01408598e88d5986ad30ef0
GiorgiZanqaidze <giorgi.zankaidze@redberry.ge>
2023-11-03 13:09:59 +0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Child1Component } from './child-1/child-1.component';
+import { Child2Component } from './child-2/child-2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Child1Component,
+    Child2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/child-2/child-2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-2',
+  templateUrl: './child-2.component.html',
+  styleUrls: ['./child-2.component.css']
+})
+export class Child2Component {
+
+}
